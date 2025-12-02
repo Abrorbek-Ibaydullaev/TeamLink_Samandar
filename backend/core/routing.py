ad1d97bf287665862cd36d5a120e63e6ca66ad45
Abrorbek-Ibaydullaev <148473615+Abrorbek-Ibaydullaev@users.noreply.github.com>
2025-12-02 20:03:33 +0100

    Delete backend directory

--- backend/core/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/projects/(?P<project_id>[^/]+)/$',
-            consumers.ProjectChatConsumer.as_asgi()),
-]
