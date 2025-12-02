0f8c75b98c89ba30b41c2a8844b96672a2a907b4
Abrorbek <abrorbekibaydullaev@gmail.com>
2025-12-02 03:57:21 +0100

    apps added with views

--- tasklink-backend/apps/projects/admin.py
+++ tasklink-backend/apps/projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, Column, ProjectMember
+admin.site.register(Project)
+admin.site.register(Column)
+admin.site.register(ProjectMember)
--- tasklink-backend/apps/tasks/admin.py
+++ tasklink-backend/apps/tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import TaskLabel, Comment, Task
 # Register your models here.
+
+# admin.site.register(Task)
+admin.site.register(TaskLabel)
+admin.site.register(Comment)
+admin.site.register(Task)
