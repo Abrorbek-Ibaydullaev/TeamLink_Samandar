aa5461074c7daeade617d75c051eb95964f513fe
Abrorbek-Ibaydullaev <148473615+Abrorbek-Ibaydullaev@users.noreply.github.com>
2026-01-20 00:13:45 +0100

    Add CORS origin for teamlink2.vercel.app

--- tasklink-backend/config/settings.py
+++ tasklink-backend/config/settings.py
@@ -125,6 +125,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
+    "https://teamlink2.vercel.app/"
 ]
 CORS_ALLOW_CREDENTIALS = True
 # Internationalization
